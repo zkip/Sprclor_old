9b8d582a1074ad4bb73d0a1848634d9a787efa53
zkip <zkiplan@qq.com>
2023-06-02 06:01:48 +0000

    add svelte project

---
+++ website/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import paper from 'paper';
+
+	onMount(() => {
+		let mx = 0,
+			my = 0;
+
+		addEventListener('mousemove', ({ clientX, clientY }) => {
+			mx = clientX;
+			my = clientY;
+			path.position.x = mx;
+			path.position.y = my;
+		});
+
+		const stage = document.querySelector('#stage') as HTMLCanvasElement;
+		paper.setup(stage);
+		const path = new paper.Path.Circle([10, 10], 10);
+		path.strokeColor = new paper.Color('red');
+	});
+</script>
+
+<canvas id="stage">not support canvas!</canvas>
